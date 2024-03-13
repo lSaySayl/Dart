1bc7adc3ce686c0e524b8187fdfd2855a9265a3f
Camilo Ramirez <juacaramirezlo@gmail.com>
2024-03-12 21:32:47 -0500

    - Revisando el tipo de dato Object, interpolación y concatenación en Dart

--- main.dart
+++ main.dart
@@ -27,7 +27,46 @@ void main() {
   value = 'Cambiando en tiempo de ejecución';
   value = false;
 
+  // Objet es la clase padre de todos los objetos en Dart, menos para nulos,
+  // Se presentan subtios de objetos como: List, Map, Set, Runes, Symbol
+  // Cada tipo será un subtipo de Object (siempre que no sea null) y podremos acceder toString, hasCode, runtimeType, noSuchMethod, etc. Rara vez se usa una variable con tipo object
+
+  var edad = 20;
+  print(edad.toString());
+  Object edadObject = 20;
+  print(edadObject.toString());
+  dynamic edadDynamic = 20;
+  print(edadDynamic.runtimeType);
+
+  // Interpolar. Para estos ejemplos no es necesario usar toString. Usamos comillas simples o dobles con signo dolar
+
+  var nombre = 'Camilo';
+  var apellido = 'Londoño';
+  var username = '$nombre $apellido $edad'; // Por detrás tengo un toString: edad.toString()
+  var username2 = "$nombre $apellido \30";
+  print(username);
+  print(username2);
+
+  // En caso de necesitar una lógica más compleja, se puede usar ${}
+
+  var username3 = '${nombre[0]} ${apellido.toUpperCase()}';
+  print(username3);
+
+  //También podemos usar el + para concatenar
+
+  var username4 = nombre + ' ' + apellido;
+  print(username4);
+
+  // Constantes o variables inmutables en Dart. Estas no pueden cambiar en tiempo de ejecución. Debemos usar la palabra reservada final.
+
+  final String nombreFinal = 'Camilo';
+  //No es necesario pasar el tipo de dato
+  final loteria = 2456;
+
+  var price = 1000.0;
+  final iva = 0.12;
+  var total = price + (price * iva);
+  print('El precio es: $price, el iva es: $iva y el total es: $total');
 
 
-  //Datos de instancia
 }
